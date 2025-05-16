1db91027b2f92bac423fe3b35298dcd5418df9a4
Vitorverg97 <vitor.vsouza05@gmail.com>
2025-05-16 13:51:38 +0000

    update - script.sql (tipo - entidade usuario)

--- script.sql
+++ script.sql
@@ -6,7 +6,7 @@ CREATE TABLE USUARIO (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    tipo ENUM('Aluno', 'Professor', 'Admin')NOT NULL
+    tipo ENUM('Aluno', 'Professor') NOT NULL
 );
 
 CREATE TABLE CURSO (
