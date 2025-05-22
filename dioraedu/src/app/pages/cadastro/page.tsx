3f46903e3cc17c6cb3346b42bde3aa8c234c671f
Vitorverg97 <vitor.vsouza05@gmail.com>
2025-05-22 16:34:51 -0300

    update - improoving the UI in cadastro and login

--- dioraedu/src/app/pages/cadastro/page.tsx
+++ dioraedu/src/app/pages/cadastro/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook, FaMicrosoft } from "react-icons/fa";
+import Image from 'next/image';
 
 export default function Cadastro() {
   const [nome, setNome] = useState('');
@@ -16,7 +17,11 @@ export default function Cadastro() {
       {/* Conteúdo principal */}
       <div className="mt-16 text-center flex flex-col items-center gap-4">
         {/* Logo */}
-        <img src="/assets/icone-cerebro.svg" alt="Logo" className="w-30 h-30" /> 
+        <Image src="/assets/icone-cerebro.svg"
+        alt="Logo-DioraEDU"
+        className="w-30 h-30"
+        width={261}
+        height={292} /> 
 
         {/* Título */}
         <h1 className="text-2xl font-bold text-black">Bem-vindo! Cadastre-se para começar</h1>
@@ -26,28 +31,28 @@ export default function Cadastro() {
           <input
             type="text"
             placeholder="Nome completo"
-            className="px-4 py-2 rounded w-full outline-none"
+            className="px-4 py-2 text-black rounded w-full outline-none"
             value={nome}
             onChange={(e) => setNome(e.target.value)}
           />
           <input
             type="text"
             placeholder="Email ou nome de usuário"
-            className="px-4 py-2 rounded w-full outline-none"
+            className="px-4 py-2 text-black rounded w-full outline-none"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             placeholder="Senha"
-            className="px-4 py-2 rounded w-full outline-none"
+            className="px-4 py-2 text-black rounded w-full outline-none"
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
           />
           <input
             type="password"
             placeholder="Confirmar senha"
-            className="px-4 py-2 rounded w-full outline-none"
+            className="px-4 py-2 text-black rounded w-full outline-none"
             value={confirmarSenha}
             onChange={(e) => setConfirmarSenha(e.target.value)}
           />
@@ -64,7 +69,7 @@ export default function Cadastro() {
           <div className="flex flex-col items-center gap-2 mt-2 w-full">
             <button
               onClick={() => window.location.href = '/pages/login'}
-              className="text-blue-400 text-sm hover:underline"
+              className="text-pink-400 text-sm hover:underline"
             >
               Já tem uma conta? Entrar
             </button>
@@ -77,15 +82,15 @@ export default function Cadastro() {
           </div>
 
           {/* Login social */}
-          <button className="flex items-center justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
+          <button className="flex items-center text-black justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
             <FcGoogle className="text-xl" />
             Continuar com o Google
           </button>
-          <button className="flex items-center justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
+          <button className="flex items-center text-black justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
             <FaFacebook className="text-blue-600 text-xl" />
             Continuar com o Facebook
           </button>
-          <button className="flex items-center justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
+          <button className="flex items-center text-black justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
             <FaMicrosoft className="text-blue-800 text-xl" />
             Continuar com a conta Microsoft
           </button>
@@ -97,8 +102,17 @@ export default function Cadastro() {
         <button className="hover:underline">Sobre nós</button>
         <button className="hover:underline">Mídias Sociais</button>
         <div className="flex gap-2">
-          <img src="/assets/google-play.png" alt="Google Play" className="h-10" />
-          <img src="/assets/app-store.png" alt="App Store" className="h-10" />
+          <Image src="/assets/google-play.png"
+          alt="Google Play"
+          className="h-10" 
+          width={130}
+          height={80}/>
+          <Image src="/assets/app-store.png"
+          alt="App Store"
+          className="h-10"
+          width={130}
+          height={80}
+          />
         </div>
       </footer>
     </div>
--- dioraedu/src/app/pages/login/page.tsx
+++ dioraedu/src/app/pages/login/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook, FaMicrosoft } from "react-icons/fa";
+import Image from 'next/image';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -14,13 +15,17 @@ export default function Login() {
       {/* Conteúdo principal */}
       <div className="mt-16 text-center flex flex-col items-center gap-4">
         {/* Logo */}
-        <img src="/assets/icone-cerebro.svg" alt="Logo" className="w-30 h-30" /> 
+        <Image src="/assets/icone-cerebro.svg"
+               alt="Logo-DioraEDU"
+               className="w-30 h-30"
+               width={261}
+               height={292} />
 
         {/* Título */}
         <h1 className="text-2xl font-bold text-black">Que bom que você voltou!</h1>
 
         {/* Formulário */}
-        <div className="flex flex-col items-center gap-3 w-80">
+        <div className="flex flex-col text-black items-center gap-3 w-80">
           <input
             type="text"
             placeholder="Email ou nome de usuário"
@@ -48,7 +53,7 @@ export default function Login() {
             <div className="flex flex-col items-center gap-2 mt-2 w-full">
             <button
                 onClick={() => window.location.href = '/pages/cadastro'}
-                className="text-blue-400 text-sm hover:underline"
+                className="text-pink-400 text-sm hover:underline"
             >
                 Não tem uma conta? Cadastrar
             </button>
@@ -62,15 +67,15 @@ export default function Login() {
 
 
           {/* Login social */}
-          <button className="flex items-center justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
+          <button className="flex items-center text-black justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
             <FcGoogle className="text-xl" />
             Continuar com o Google
           </button>
-          <button className="flex items-center justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
+          <button className="flex items-center text-black justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
             <FaFacebook className="text-blue-600 text-xl" />
             Continuar com o Facebook
           </button>
-          <button className="flex items-center justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
+          <button className="flex items-center text-black justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
             <FaMicrosoft className="text-blue-800 text-xl" />
             Continuar com a conta Microsoft
           </button>
@@ -82,8 +87,20 @@ export default function Login() {
         <button className="hover:underline">Sobre nós</button>
         <button className="hover:underline">Mídias Sociais</button>
         <div className="flex gap-2">
-          <img src="/assets/google-play.png" alt="Google Play" className="h-10" />
-          <img src="/assets/app-store.png" alt="App Store" className="h-10" />
+          
+        <Image src="/assets/google-play.png"
+               alt="Google Play"
+               className="h-10" 
+               width={130}
+               height={80}
+        />
+          
+        <Image src="/assets/app-store.png"
+               alt="App Store"
+               className="h-10"
+               width={130}
+               height={80}
+        />
         </div>
       </footer>
     </div>
