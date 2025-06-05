9802d37fbd93071b7359fd5e88f4cfa7ea1cc690
Vitorverg97 <vitor.vsouza05@gmail.com>
2025-06-05 12:05:20 -0300

    update - Improoving the logic of the login page

--- dioraedu/src/app/pages/login/page.tsx
+++ dioraedu/src/app/pages/login/page.tsx
@@ -4,10 +4,29 @@ import { useState } from 'react'
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook, FaMicrosoft } from "react-icons/fa";
 import Image from 'next/image';
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [erro, setErro] = useState("");
+  const router = useRouter();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const res = await signIn("credentials", {
+      redirect: false,
+      email,
+      senha,
+    });
+
+    if (res?.error) {
+      setErro("E-mail ou senha inválidos");
+    } else {
+      router.push("/dashboard");
+    }
+  }
 
   return (
         <div className="min-h-screen bg-[#BDE3FA] text-black flex flex-col justify-between">
@@ -25,45 +44,52 @@ export default function Login() {
 
         {/* Formulário */}
         <div className="flex flex-col text-black items-center gap-3 w-80">
+          <form onSubmit={handleSubmit} className="max-w-sm  mt-10">
           <input
-            type="text"
-            placeholder="Email ou nome de usuário"
-            className="px-4 py-2 rounded w-full outline-none"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="E-mail"
+            type="email"
+            className="px-4 py-2 rounded w-full outline-none input" required
+            
           />
+          
           <input
-            type="password"
-            placeholder="Senha"
-            className="px-4 py-2 rounded w-full outline-none"
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
+            placeholder="Senha"
+            type="password"
+            className="px-4 py-2 rounded w-full outline-none input" required
           />
+          
+          {erro && <div className='text-red-500 mt-2'>{erro}</div>}
+
           <button
-            onClick={() => {
-              window.location.href = "/pages/home-aluno";
-            }}
+            type="submit"
             className="bg-[#4A4A4A] text-white px-6 py-2 rounded w-full hover:opacity-90"
           >
             Continuar
           </button>
-
+          
 
             <div className="flex flex-col items-center gap-2 mt-2 w-full">
             <button
-                onClick={() => window.location.href = '/pages/cadastro'}
-                className="text-pink-400 text-sm hover:underline"
+              type='button'
+              className="text-pink-400 text-sm hover:underline"
+              onClick={() => router.push("/pages/cadastro")}
             >
-                Não tem uma conta? Cadastrar
+              Não tem uma conta? Cadastrar
             </button>
-
-            <div className="flex items-center justify-center w-full text-black">
-                <div className="border-t border-black flex-grow mr-2"></div>
-                <span className="text-sm">ou</span>
-                <div className="border-t border-black flex-grow ml-2"></div>
             </div>
+            </form>
+          
+          <div className="flex items-center justify-center w-full text-black">
+                <div className="flex-grow mr-2"></div>
+                <span className="text-black">ou</span>
+                <div className="flex-grow ml-2"></div>
+            
+            
             </div>
-
 
           {/* Login social */}
           <button className="flex items-center text-black justify-center gap-3 border px-4 py-2 rounded w-full bg-white hover:bg-gray-100">
